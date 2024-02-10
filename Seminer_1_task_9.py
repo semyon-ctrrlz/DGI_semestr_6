2cfcc5992a707dbc1dacdb19f68f2a6bffbe826c
Semen <samsr102@bk.ru>
2024-02-10 13:31:11 +0300

    task9: well done

---
+++ Seminer_1_task_9.py
@@ -0,0 +1,9 @@
+numbers=['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+print('Вышел зайчик погулять')
+
+numbers=['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+    print('Вышел зайчик погулять')
