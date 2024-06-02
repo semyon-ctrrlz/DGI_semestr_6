a9ef7fe3da378311e56702a6673bbd806104953f
semyon-ctrrlz <64481704+semyon-ctrrlz@users.noreply.github.com>
2024-06-02 19:38:05 +0300

    У меня видимо не сохранился тогда с смеинара

--- client.py
+++ client.py
@@ -1,11 +1,33 @@
 import socket
+from _thread import start_new_thread
 
-client = socket.socket()
+client= socket.socket()
 
-client.connect(('192.168.210.86', 3000))
+client.connect(('192.168.210.163', int(3000)))
 
 data = client.recv(1024)
-print('Сообщение в бинарном виде', data)
-print('Сообщение в бинарном виде', data.decode())
+print('code:', data)
+print('message:', data.decode())
+
+message = 'за карш не скину'
+data = message.encode()
+client.send(data)
+
+def getMessages():
+    while True:
+        data = client.recv(1024)
+        print('Input', data.decode())
+        if data.decode() == 'Close!':
+            break
+    client.close()
+
+start_new_thread(getMessages, ())    
+
+while True:
+    message = input('Input your message:')
+    data = message.encode()
+    client.send(data)
+    if message == 'Close!':
+        break
 
 client.close()
